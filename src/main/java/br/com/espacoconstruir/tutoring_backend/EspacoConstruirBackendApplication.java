75c04391d5ca790be91a46c87ab16e6630aa84db
Adriano <adrianosouzaa733@gmail.com>
2025-05-22 00:19:55 -0300

    enviando base do spring boot

---
+++ src/main/java/br/com/espacoconstruir/tutoring_backend/EspacoConstruirBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.espacoconstruir.tutoring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspacoConstruirBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspacoConstruirBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/espacoconstruir/tutoring_backend/EspacoConstruirBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.espacoconstruir.tutoring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspacoConstruirBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
