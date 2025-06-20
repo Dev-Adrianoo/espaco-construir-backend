c95c1860885bc0dc4df33dbbcae91847222cad78
adriano-notebook <adrianosouzaa733@gmail.com>
2025-06-20 17:43:29 -0300

    feat: mudança de sintaxy nas migrations e mudança na aplicação para java 21

--- src/main/resources/db/migration/V7__Create_Class_History_Table.sql
+++ src/main/resources/db/migration/V7__Create_Class_History_Table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE  IF NOT EXISTS  class_history (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     student_id BIGINT NOT NULL,
     teacher_id BIGINT NOT NULL,
     class_id BIGINT,
     comment TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_classhistory_student FOREIGN KEY (student_id) REFERENCES users(id),
     CONSTRAINT fk_classhistory_teacher FOREIGN KEY (teacher_id) REFERENCES users(id),
-    CONSTRAINT fk_classhistory_class FOREIGN KEY (class_id) REFERENCES class(id)
+    CONSTRAINT fk_classhistory_class FOREIGN KEY (class_id) REFERENCES classes(id)
 ); 
