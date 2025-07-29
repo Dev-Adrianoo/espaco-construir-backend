267878d9208148a0e98b2e6627a9818f9c6792c6
Adriano-pc <adrianosouzaa733@gmail.com>
2025-07-29 18:27:12 -0300

    fix: endpoints, rodas para produção

--- src/main/java/br/com/espacoconstruir/tutoring_backend/config/SecurityConfig.java
+++ src/main/java/br/com/espacoconstruir/tutoring_backend/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
             .requestMatchers("/teachers").hasAnyAuthority("RESPONSAVEL", "PROFESSORA")
             
             // 6. Qualquer outra rota precisa estar autenticada
-            .anyRequest().permitAll()
+            .anyRequest().authenticated()
         )
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
