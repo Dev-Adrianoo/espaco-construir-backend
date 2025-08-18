c6e1bf5bf2f36a39b725c8f1e23e96d8f5d54cb9
Adriano-pc <adrianosouzaa733@gmail.com>
2025-08-18 20:03:52 -0300

    fix: adicionando configuração de segurança para ser uma prioridade

--- src/main/java/br/com/espacoconstruir/tutoring_backend/config/ForwardedHeaderConfig.java
+++ src/main/java/br/com/espacoconstruir/tutoring_backend/config/ForwardedHeaderConfig.java
@@ -1,14 +1,19 @@
 package br.com.espacoconstruir.tutoring_backend.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 public class ForwardedHeaderConfig {
 
     @Bean
-    public ForwardedHeaderFilter forwardedHeaderFilter() {
-        return new ForwardedHeaderFilter();
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        FilterRegistrationBean<ForwardedHeaderFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new ForwardedHeaderFilter());
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
